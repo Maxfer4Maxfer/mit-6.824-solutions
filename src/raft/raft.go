459397e1331ebe10413579e1dee6d098dd565fc3
Maxim Ferapontov <maxim@Maxims-MBP.Dlink>
2022-07-06 20:09:26 +0300

    Raft 2B

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -16,11 +16,8 @@ package raft
 
 import (
 	"context"
-	crand "crypto/rand"
 	"fmt"
 	"log"
-	"math/big"
-	"math/rand"
 	"os"
 	"sync"
 	"sync/atomic"
@@ -57,27 +54,6 @@ type ApplyMsg struct {
 	SnapshotIndex int
 	SnapshotValid bool
 	Snapshot      []byte
-}
-
-type state int32
-
-const (
-	followerState state = iota
-	candidateState
-	leaderState
-)
-
-func (s state) String() string {
-	switch s {
-	case followerState:
-		return "FOLLOWER"
-	case candidateState:
-		return "CANDIDATE"
-	case leaderState:
-		return "LEADER"
-	default:
-		return "NONE"
-	}
 }
 
 type LogEntry struct {
@@ -177,7 +153,9 @@ func (rf *Raft) readPersist(data []byte) {
 
 // A service wants to switch to snapshot.  Only do so if Raft hasn't
 // have more recent info since it communicate the snapshot on applyCh.
-func (rf *Raft) CondInstallSnapshot(lastIncludedTerm int, lastIncludedIndex int, snapshot []byte) bool {
+func (rf *Raft) CondInstallSnapshot(
+	lastIncludedTerm int, lastIncludedIndex int, snapshot []byte,
+) bool {
 	// Your code here (2D).
 	return true
 }
@@ -210,14 +188,14 @@ func (rf *Raft) RequestVote(
 	case args.Term < rf.currentTerm:
 		log.Printf("Incoming term smaller %d < %d", args.Term, rf.currentTerm)
 	case rf.votedFor != -1:
-		log.Printf("Already voted for %d ", rf.votedFor)
+		log.Printf("Already voted for %d", rf.votedFor)
 	case rf.votedFor == args.CandidateID:
-		log.Printf("Already voted for incoming candidate %d ", rf.votedFor)
+		log.Printf("Already voted for incoming candidate %d", rf.votedFor)
 	// candidate’s log is at least as up-to-date as receiver’s log
-	case len(rf.log) < args.LastLogIndex:
-		log.Printf("Has smaller log %d != %d ", len(rf.log), args.LastLogIndex)
+	case len(rf.log)-1 < args.LastLogIndex:
+		log.Printf("Has smaller log size %d != %d", len(rf.log)-1, args.LastLogIndex)
 	case rf.log[args.LastLogIndex].Term != args.LastLogTerm:
-		log.Printf("has log discrepancy %d != %d  ",
+		log.Printf("has log discrepancy %d != %d",
 			rf.log[args.LastLogIndex].Term, args.LastLogTerm)
 	default:
 		reply.VoteGranted = true
@@ -268,6 +246,9 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	isLeader := true
 
 	// Your code here (2B).
+	if !rf.heartbeats.IsSendingInProgress() {
+		return -1, -1, false
+	}
 
 	return index, term, isLeader
 }
@@ -335,169 +316,6 @@ func (rf *Raft) processIncomingTerm(log *log.Logger, peerID int, term int) {
 
 		rf.votedFor = -1
 		rf.currentTerm = term
-	}
-}
-
-func (rf *Raft) askForVoting(
-	ctx context.Context, log *log.Logger,
-	peer int, args *RequestVoteArgs, resCh chan bool,
-) {
-	reply := &RequestVoteReply{}
-
-	log.Printf("Sending RequestVote to S%d", peer)
-
-	if ok := rf.sendRequestVote(peer, args, reply); !ok {
-		log.Printf("WRN fail RequestVote call to S%d peer", peer)
-	}
-
-	log.Printf("Recived RequestVoteReply from S%d", peer)
-
-	rf.processIncomingTerm(log, args.CandidateID, args.Term)
-
-	var res bool
-
-	if reply.VoteGranted {
-		log.Printf("Win: S%d voted for us", peer)
-
-		res = true
-	} else {
-		log.Printf("Loose: S%d voted against us", peer)
-
-		res = false
-	}
-
-	select {
-	case <-ctx.Done():
-		log.Printf("Stop ask S%d for voining", peer)
-
-		return
-	default:
-		resCh <- res
-	}
-}
-
-func (rf *Raft) prepareForVote(log *log.Logger) *RequestVoteArgs {
-	rf.mu.Lock()
-	defer rf.mu.Unlock()
-
-	rf.currentTerm++
-	rf.votedFor = rf.me
-
-	log.Printf("Become a condidate and increase term to %d", rf.currentTerm)
-
-	return &RequestVoteArgs{
-		Term:         rf.currentTerm,
-		CandidateID:  rf.me,
-		LastLogIndex: len(rf.log) - 1,
-		LastLogTerm:  rf.log[len(rf.log)-1].Term,
-	}
-}
-
-func (rf *Raft) startLeaderElection() {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	rf.mu.Lock()
-	rf.electionCancelFunc = cancel
-	rf.mu.Unlock()
-
-	rf.leaderElection(ctx)
-}
-
-func (rf *Raft) stopLeaderElection() {
-	rf.electionCancelFunc()
-}
-
-func (rf *Raft) leaderElection(ctx context.Context) {
-	log := extendLoggerWithPrefix(rf.logger, leaderElectionLogTopic)
-
-	log.Printf("Starting new Leader election cycle")
-
-	args := rf.prepareForVote(log)
-	nVoted := 1              // already voted for themselves
-	nVotedFor := 1           // already voted for themselves
-	resCh := make(chan bool) // true - votedFor; false - votedAgeinst
-
-	for i := range rf.peers {
-		if i == rf.me {
-			continue
-		}
-
-		go rf.askForVoting(ctx, log, i, args.DeepCopy(), resCh)
-	}
-
-	for {
-		select {
-		case <-ctx.Done():
-			log.Print("Stop election (term sig)")
-			return
-		case res := <-resCh:
-			nVoted++
-
-			if res {
-				nVotedFor++
-			}
-
-			log.Printf("(%d/%d) voted, %d for, %d against",
-				nVoted, len(rf.peers), nVotedFor, nVoted-nVotedFor)
-
-			if nVotedFor > len(rf.peers)/2 || nVoted == len(rf.peers) {
-				if nVotedFor > len(rf.peers)/2 {
-					log.Print("I am a new leader")
-					rf.heartbeats.StartSending()
-				} else {
-					log.Print("Not luck, going to try the next time")
-					rf.mu.Lock()
-					rf.votedFor = -1
-					rf.mu.Unlock()
-				}
-
-				return
-			}
-		}
-	}
-}
-
-// The ticker go routine starts a new election if this peer hasn't received
-// heartsbeats recently.
-func (rf *Raft) ticker() {
-	log := extendLoggerWithPrefix(rf.logger, tickerLogTopic)
-
-	makeSeed := func() int64 {
-		max := big.NewInt(int64(1) << 62)
-		bigx, _ := crand.Int(crand.Reader, max)
-		x := bigx.Int64()
-
-		return x
-	}
-
-	rand.Seed(makeSeed())
-
-	for !rf.killed() {
-		// (2A) Your code here to check if a leader election should
-		// be started and to randomize sleeping time using
-		// time.Sleep().
-		rf.mu.Lock()
-		isLeader := rf.heartbeats.IsSendingInProgress()
-		longAlone := rf.lastHeartbeat.Add(rf.electionTimeout).Before(time.Now())
-		rf.mu.Unlock()
-
-		if !isLeader && longAlone {
-			log.Printf("Did not see a leader to much")
-
-			rf.startLeaderElection()
-		}
-
-		max := int64(electionTimeoutUpperBoundary)
-		min := int64(electionTimeoutLowerBoundary)
-		et := time.Duration(rand.Int63n(max-min) + min)
-
-		log.Printf("Set a new election timeout %v", et)
-
-		rf.mu.Lock()
-		rf.electionTimeout = et
-		rf.mu.Unlock()
-
-		time.Sleep(et)
 	}
 }
 
---
+++ src/raft/raft_election.go
@@ -0,0 +1,173 @@
+package raft
+
+import (
+	"context"
+	crand "crypto/rand"
+	"log"
+	"math/big"
+	"math/rand"
+	"time"
+)
+
+func (rf *Raft) askForVoting(
+	ctx context.Context, log *log.Logger,
+	peer int, args *RequestVoteArgs, resCh chan bool,
+) {
+	reply := &RequestVoteReply{}
+
+	log.Printf("Sending RequestVote to S%d", peer)
+
+	if ok := rf.sendRequestVote(peer, args, reply); !ok {
+		log.Printf("WRN fail RequestVote call to S%d peer", peer)
+	}
+
+	log.Printf("Recived RequestVoteReply from S%d", peer)
+
+	rf.processIncomingTerm(log, args.CandidateID, args.Term)
+
+	var res bool
+
+	if reply.VoteGranted {
+		log.Printf("Win: S%d voted for us", peer)
+
+		res = true
+	} else {
+		log.Printf("Loose: S%d voted against us", peer)
+
+		res = false
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Printf("Stop ask S%d for voining", peer)
+
+		return
+	default:
+		resCh <- res
+	}
+}
+
+func (rf *Raft) prepareForVote(log *log.Logger) *RequestVoteArgs {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.currentTerm++
+	rf.votedFor = rf.me
+
+	log.Printf("Become a condidate and increase term to %d", rf.currentTerm)
+
+	return &RequestVoteArgs{
+		Term:         rf.currentTerm,
+		CandidateID:  rf.me,
+		LastLogIndex: len(rf.log) - 1,
+		LastLogTerm:  rf.log[len(rf.log)-1].Term,
+	}
+}
+
+func (rf *Raft) startLeaderElection() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	rf.mu.Lock()
+	rf.electionCancelFunc = cancel
+	rf.mu.Unlock()
+
+	rf.leaderElection(ctx)
+}
+
+func (rf *Raft) stopLeaderElection() {
+	rf.electionCancelFunc()
+}
+
+func (rf *Raft) leaderElection(ctx context.Context) {
+	log := extendLoggerWithPrefix(rf.logger, leaderElectionLogTopic)
+
+	log.Printf("Starting new Leader election cycle")
+
+	args := rf.prepareForVote(log)
+	nVoted := 1              // already voted for themselves
+	nVotedFor := 1           // already voted for themselves
+	resCh := make(chan bool) // true - votedFor; false - votedAgeinst
+
+	for i := range rf.peers {
+		if i == rf.me {
+			continue
+		}
+
+		go rf.askForVoting(ctx, log, i, args.DeepCopy(), resCh)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Print("Stop election (term sig)")
+			return
+		case res := <-resCh:
+			nVoted++
+
+			if res {
+				nVotedFor++
+			}
+
+			log.Printf("(%d/%d) voted, %d for, %d against",
+				nVoted, len(rf.peers), nVotedFor, nVoted-nVotedFor)
+
+			if nVotedFor > len(rf.peers)/2 || nVoted == len(rf.peers) {
+				if nVotedFor > len(rf.peers)/2 {
+					log.Print("I am a new leader")
+					rf.heartbeats.StartSending()
+				} else {
+					log.Print("Not luck, going to try the next time")
+					rf.mu.Lock()
+					rf.votedFor = -1
+					rf.mu.Unlock()
+				}
+
+				return
+			}
+		}
+	}
+}
+
+// The ticker go routine starts a new election if this peer hasn't received
+// heartsbeats recently.
+func (rf *Raft) ticker() {
+	log := extendLoggerWithPrefix(rf.logger, tickerLogTopic)
+
+	makeSeed := func() int64 {
+		max := big.NewInt(int64(1) << 62)
+		bigx, _ := crand.Int(crand.Reader, max)
+		x := bigx.Int64()
+
+		return x
+	}
+
+	rand.Seed(makeSeed())
+
+	for !rf.killed() {
+		// (2A) Your code here to check if a leader election should
+		// be started and to randomize sleeping time using
+		// time.Sleep().
+		rf.mu.Lock()
+		isLeader := rf.heartbeats.IsSendingInProgress()
+		longAlone := rf.lastHeartbeat.Add(rf.electionTimeout).Before(time.Now())
+		rf.mu.Unlock()
+
+		if !isLeader && longAlone {
+			log.Printf("Did not see a leader to much")
+
+			rf.startLeaderElection()
+		}
+
+		max := int64(electionTimeoutUpperBoundary)
+		min := int64(electionTimeoutLowerBoundary)
+		et := time.Duration(rand.Int63n(max-min) + min)
+
+		log.Printf("Set a new election timeout %v", et)
+
+		rf.mu.Lock()
+		rf.electionTimeout = et
+		rf.mu.Unlock()
+
+		time.Sleep(et)
+	}
+}
